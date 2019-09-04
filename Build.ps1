f28922f51aa64c9f931d6f42085028f2143875db
Dmitry Ignatiev <lovesan.ru@gmail.com>
2019-09-04 19:10:13 +0300

    test

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,5 @@
 $env:PATH="C:/Program Files (x86)/MSBuild/14.0/Bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin;${env:PATH}"
 choco install vswhere
+echo "MSBuild is:"
 vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+echo '^^^'
