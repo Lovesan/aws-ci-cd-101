dad24543b1790ca817a598e567714637681fad30
Dmitry Ignatiev <lovesan.ru@gmail.com>
2019-09-12 19:32:00 +0300

    Clear deployment script

--- scripts/AfterInstall.ps1
+++ scripts/AfterInstall.ps1
@@ -1,10 +1,1 @@
 $ErrorActionPreference = 'Stop'
-
-Import-Module WebAdministration
-
-$name = 'AwsCICD101'
-$path = "C:\aws-ci-cd-101"
-
-if (-not (Get-Website $name)) {
-  New-Website -Name $name -Port 80 -PhysicalPath $path -Force
-}
