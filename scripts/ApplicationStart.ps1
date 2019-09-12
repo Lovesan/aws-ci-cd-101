cd992a5cbef296ed652a08ca40e9f1ecc590675b
Dmitry Ignatiev <lovesan.ru@gmail.com>
2019-09-12 19:46:22 +0300

    Restart as 64-bit powershell

--- scripts/ApplicationStart.ps1
+++ scripts/ApplicationStart.ps1
@@ -1,1 +1,23 @@
 $ErrorActionPreference = 'Stop'
+
+# Are you running in 32-bit mode?
+#   (\SysWOW64\ = 32-bit mode)
+
+if ($PSHOME -like "*SysWOW64*")
+{
+  Write-Warning "Restarting this script under 64-bit Windows PowerShell."
+
+  # Restart this script under 64-bit Windows PowerShell.
+  #   (\SysNative\ redirects to \System32\ for 64-bit mode)
+
+  & (Join-Path ($PSHOME -replace "SysWOW64", "SysNative") powershell.exe) -File `
+    (Join-Path $PSScriptRoot $MyInvocation.MyCommand) @args
+
+  # Exit 32-bit script.
+
+  Exit $LastExitCode
+}
+
+Import-Module WebAdministration
+
+Start-Website -Name AwsCICD101
